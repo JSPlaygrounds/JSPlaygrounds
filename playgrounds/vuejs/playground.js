24a988ad7318e257d31f18beb9ca95e7c440f21b
thuey <thuey100@gmail.com>
2017-04-24 22:06:31 -0600

    wrapped view bootstrap in window.onload

--- playgrounds/vuejs/playground.js
+++ playgrounds/vuejs/playground.js
@@ -1,6 +1,8 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue'
-  }
-});
+window.onload = function() {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue'
+    }
+  });
+};
